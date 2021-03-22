89f43654dab1dd1b37465de41cb40e0596205e4a
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-03-23 03:55:55 +0530

    reply if unknown command is used

--- cogs/ready.py
+++ cogs/ready.py
@@ -12,6 +12,11 @@ class ready(commands.Cog):
 		await self.client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="my Homies."))
 		print(f'I am ready to serve.')
 
+	# Unknown commands
+	@commands.Cog.listener()
+	async def on_command_error(self, ctx, error):
+		if isinstance(error, commands.CommandNotFound):
+			await ctx.send('Unknown command used.')
 
 def setup(client):
 	client.add_cog(ready(client))
