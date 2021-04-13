bfbf9cfa63da447c58586dff32441413f1177dcf
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-04-13 10:30:23 +0530

    music: don't download just stream

--- cogs/music.py
+++ cogs/music.py
@@ -1,10 +1,15 @@
 from discord.ext import commands, tasks
 from discord.utils import get
 from discord import FFmpegPCMAudio, Activity, ActivityType, Status
-from os.path import isfile
-from os import remove, listdir, rename
-from olmusic import check_urls, yt_download, spotify
-from re import sub
+from olmusic import check_urls
+import youtube_dl.YoutubeDL as YDL
+ydl_opts = {
+    'noplaylist': True,
+    'quiet': True,
+    'no_warnings': True,
+    'format': 'bestaudio/best',
+}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 song_title=''
 
 class music(commands.Cog):
@@ -14,55 +19,41 @@ class music(commands.Cog):
     @commands.command(pass_context=True, brief='This will play a song .play [url]', aliases=['p'])
     async def play(self, ctx,*,url:str=''):
         global song_title
+        
+        # Check if author in VC
+        if ctx.author.voice is None or ctx.author.voice.channel is None:
+            return await ctx.send("You are not connected to a voice channel.")
+        # Check da url for bad stuff
         if check_urls(url):
             return await ctx.send('Thats not a fucking song.')
-        song_there = isfile('song.mp3')
-        try:
-            if song_there:
-                remove('song.mp3')
-        except PermissionError:
-            if ctx.voice_client.is_paused() is True:
-                await ctx.send(f'Resuming: `{song_title}`')
-                return await ctx.voice_client.resume()
-            else:
-                return await ctx.send('Wait for the current playing music to end or use the \'.stop\' command')
-        print(f'Connecting to {ctx.message.author.voice.channel}')
-        voiceChannel = ctx.message.author.voice.channel
-        if not voiceChannel:
-            await ctx.send("You are not connected to a voice channel.")
-            return
+        # If player is_paused resume...
+        if url=='' and ctx.voice_client.is_paused() is True:
+            await ctx.send(f'Resuming: `{song_title}`')
+            music.status_set.start(self, ctx)
+            return await ctx.voice_client.resume()
+        # Join/Move to vc
         voice = get(self.client.voice_clients, guild=ctx.guild)
-
+        voiceChannel = ctx.message.author.voice.channel
         if voice and voice.is_connected():
             await voice.move_to(voiceChannel)
         else:
             voice = await voiceChannel.connect()
-
+        # If is_playing gtfo
+        if voice and voice.is_playing() is True:
+            return await ctx.send('Wait for the current playing music to end or use the \'.stop\' command')
+  
+        print(f'Connecting to {ctx.message.author.voice.channel}')
         reply = await ctx.send('Loading...')
-        try:
-            yt_download(url)
-            for file in listdir('./'):
-                if file.endswith('.mp3'):
-                    song_title = ''
-                    song_title += file[:-16]
-                    song_title = sub("[\(\[].*?[\)\]]", "", song_title)
-                    print(f'Playing: {song_title}')
-                    rename(file, 'song.mp3')
-            voice.play(FFmpegPCMAudio('song.mp3'))
-            await reply.edit(content=f'Playing: `{song_title}`')
-            music.status_set.start(self, ctx)
-        except:
-            spotify(url)
-            for file in listdir('./'):
-                if file.endswith('.mp3'):
-                    song_title = ''
-                    song_title += file[:-4]
-                    song_title = sub("[\(\[].*?[\)\]]", "", song_title)
-                    print(f'Playing: {song_title}')
-                    rename(file, 'song.mp3')
-            voice.play(FFmpegPCMAudio('song.mp3'))
-            await reply.edit(content=f'Playing: `{song_title}`')
-            music.status_set.start(self, ctx)
+        with YDL(ydl_opts) as ydl:
+            try:
+                song_info = ydl.extract_info(url, download=False)
+            except:
+                song_info = ydl.extract_info(f'ytsearch:{url}', download=False)['entries'][0]
+            songurl=song_info["formats"][0]["url"]
+            song_title = song_info.get('title', None)
+        voice.play(FFmpegPCMAudio(songurl, **FFMPEG_OPTIONS))
+        await reply.edit(content=f'Playing: `{song_title}`')
+        music.status_set.start(self, ctx)
             
     @tasks.loop(seconds = 5)
     async def status_set(self, ctx):
@@ -78,7 +69,7 @@ class music(commands.Cog):
         if ctx.message.author.voice is None:
             return await ctx.send('You must be is same VC as the bot.')
         if ctx.voice_client is None:
-            return await ctx.send('Bot is not connect to a VC.')
+            return await ctx.send('Bot is not connect to VC.')
         if ctx.message.author.voice is not None and ctx.voice_client is not None:
             if ctx.voice_client.is_playing() is True or ctx.voice_client.is_paused() is True or ctx.voice_client is not None:
                 ctx.voice_client.stop()
@@ -90,7 +81,7 @@ class music(commands.Cog):
         if ctx.message.author.voice is None:
             return await ctx.send('You must be is same VC as the bot.')
         if ctx.voice_client is None:
-            return await ctx.send('Bot is not connect to a VC.')
+            return await ctx.send('Bot is not connect to VC.')
         if ctx.message.author.voice is not None and ctx.voice_client is not None:
             if ctx.voice_client.is_paused() is False:
                 ctx.voice_client.pause()
--- olmusic.py
+++ olmusic.py
@@ -4,31 +4,6 @@ def check_urls(url):
 		if badurl in url:
 			return True
 
-import youtube_dl.YoutubeDL as YDL
-def yt_download(url):
-	ydl_opts = {
-    'format': 'bestaudio/best',
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-	'quiet': True,
-    'no_warnings': True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-	with YDL(ydl_opts) as ydl:
-		ydl.download([url])
-
-from os import system
-def spotify(name):
-	print('Searching in spotify...')
-	song=name.replace(' ','_')
-	system('spotdl -o D:\DiscordBOt\Assistant '+ f'{song}')
-
 def listToString(s):
 	s[0]=f'👉\t' + s[0]
 	str1='\n👉\t'
