13937fe63c1725ca157446c4e14cf8b14560ceba
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-04-15 09:58:12 +0530

    improve bad song deduction

--- olmusic.py
+++ olmusic.py
@@ -1,8 +1,7 @@
 def check_urls(url):
-	badstuff=['dQw4w9WgXcQ','eYq7WapuDLU', 'enjoy enjami', 'enjaami', 'rick roll', 'never gonna give you up'] #songs i don't like
-	for badurl in badstuff:
-		if badurl in url:
-			return True
+	badstuff=['dQw4w9WgXcQ','eYq7WapuDLU', 'enjami', 'enjaami', 'rick roll', 'never gonna give'] #songs i don't like
+	if any(word in url.lower().split() for word in badstuff):
+		return True
 
 def listToString(s):
 	s[0]=f'👉\t' + s[0]
