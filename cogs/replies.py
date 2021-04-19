0fedd3462b4be6ebeb6e2223b96a5f636a8afe74
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-04-19 08:15:43 +0530

    GEt DM and dump

--- cogs/replies.py
+++ cogs/replies.py
@@ -1,6 +1,7 @@
 from random import choice
 import discord.ext.commands as commands
 from olreplies import *
+from olenv import DM_Channel
 
 class replies(commands.Cog):
 
@@ -12,6 +13,9 @@ class replies(commands.Cog):
 	async def on_message(self, message):
 		if message.author == self.client.user:
 			return
+		if message.guild is None and not message.author.bot:
+			channel = self.client.get_channel(DM_Channel)
+			await channel.send(f'UserID: `{message.author.id}`\nMessage Author: `{message.author}`\nMessage:\n`{message.content}`\n──────────────────────────────')
 		if any(word in message.content.lower().split() for word in hiMsgs):
 			response = choice(hiMsgReplys)
 			await message.channel.send(response)
@@ -24,8 +28,6 @@ class replies(commands.Cog):
 			response = choice(yeahMsgReplys)
 			await message.channel.send(response)
 			await self.client.process_commands(message)
-
-
 
 def setup(client):
 	client.add_cog(replies(client))
