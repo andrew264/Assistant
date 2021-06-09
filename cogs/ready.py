f49044732d58b395e5de1db08648010cf768a820
Andrew Santhosh <andrewsanthosh264@gmail.com>
2021-06-09 10:18:19 +0530

    fancy console

--- cogs/ready.py
+++ cogs/ready.py
@@ -1,5 +1,6 @@
 from discord import Activity, ActivityType, Status
-import discord.ext.commands as commands
+from discord.ext import commands, tasks
+import os
 
 class ready(commands.Cog):
 
@@ -9,10 +10,36 @@ class ready(commands.Cog):
 	# Start
 	@commands.Cog.listener()
 	async def on_ready(self):
-		await self.client.change_presence(status=Status.idle, activity=Activity(type=ActivityType.watching, name="my Homies."))
+		await self.client.change_presence(status=Status.online, activity=Activity(type=ActivityType.watching, name="andrew than vararuu....maadu pudika poraru.."))
+		if ready.leguilds.is_running() is False:
+			ready.leguilds.start(self)
+
+	# 
+	@tasks.loop(seconds = 1)
+	async def leguilds(self):
+		clear = lambda: os.system('cls')
+		clear()
 		print(f'{self.client.user} is connected to the following guild:\n')
 		for guild in self.client.guilds:
 			print(f'\t{guild.name} (ID: {guild.id}) (Member Count: {guild.member_count})')
+			
+		print('\nPeople in VC:')
+		for guild in self.client.guilds:
+			for vc in guild.voice_channels:
+				if vc.members!=[]:
+					print(f'\t{vc.name}:')
+				for members in vc.members:
+					str1=f'{members.name} '
+					if members.voice.self_mute is False:
+						str1 += '🎤'
+					else: str1 += '🙊'
+					if members.voice.self_deaf is False:
+						str1 += '🎧'
+					else: str1 += '🙉'
+					if members.voice.self_stream is True:
+						str1 += ' is live'
+					print(f'\t\t{str1}')
+
 
 	# Unknown commands
 	@commands.Cog.listener()
