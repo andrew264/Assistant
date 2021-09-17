6ea685b6d0ae7000a52076759ff8103283d47102
andrew264 <andrewsanthosh264@gmail.com>
2021-09-17 11:06:07 +0530

    private: instead of deleting just hide them <3

--- cogs/private.py
+++ cogs/private.py
@@ -2,7 +2,6 @@
 import discord
 import discord.ext.commands as commands
 from dislash.application_commands import slash_client
-from dislash.interactions.application_command import Option, OptionType
 from dislash.interactions.app_command_interaction import SlashInteraction
 
 class private(commands.Cog):
@@ -17,29 +16,37 @@ class private(commands.Cog):
 
 	@chat.sub_command(description="Create a new Private Chat.")
 	async def create(self, inter: SlashInteraction):
-		for i in inter.guild.categories:
-				if i.name == f"{inter.author.display_name}'s Chat":
-					return await inter.reply("Don't you already have one ?", ephemeral=True)
-		overwrites = {
+		overwrites_readEnable = discord.PermissionOverwrite()
+		overwrites_readEnable.read_messages = True
+		for category in inter.guild.categories:
+			if category.name == f"{inter.author.display_name}'s Chat" and isinstance(inter.author, discord.Member):
+				for channel in category.channels:
+					await channel.set_permissions(inter.author, overwrite=overwrites_readEnable)
+				await category.set_permissions(inter.author, overwrite=overwrites_readEnable)
+				return await inter.reply(f"Created a new Private Chat ({category.mention}).", ephemeral=True)
+		overwrites_readTrue = {
 			inter.guild.default_role: discord.PermissionOverwrite(read_messages=False),
 			inter.guild.me: discord.PermissionOverwrite(read_messages=True),
 			inter.author: discord.PermissionOverwrite(read_messages=True)
 		}
-		category: discord.CategoryChannel = await inter.guild.create_category(f"{inter.author.display_name}'s Chat", overwrites=overwrites, reason=None, position=None)
-		await category.create_text_channel("Chat", overwrites=overwrites)
-		await category.create_voice_channel("Private Voice", overwrites=overwrites)
+		category: discord.CategoryChannel = await inter.guild.create_category(f"{inter.author.display_name}'s Chat", overwrites=overwrites_readTrue, reason=None, position=None)
+		await category.create_text_channel("Chat", overwrites=overwrites_readTrue)
+		await category.create_voice_channel("Private Voice", overwrites=overwrites_readTrue)
 		await inter.reply(f"Created a new Private Chat ({category.mention}).", ephemeral=True)
 
 	@chat.sub_command(description="Delete existing Private Chat.")
 	async def delete(self, inter: SlashInteraction):
+		overwrites_readFalse = discord.PermissionOverwrite()
+		overwrites_readFalse.read_messages = False
+		category = None
 		for i in inter.guild.categories:
 			if i.name == f"{inter.author.display_name}'s Chat":
-				category: discord.CategoryChannel = i
+				category = i
 			else: category = None
-		if category is not None:
+		if isinstance(category, discord.CategoryChannel) and isinstance(inter.author, discord.Member):
 			for channel in category.channels:
-				await channel.delete()
-			await category.delete()
+				await channel.set_permissions(inter.author, overwrite=overwrites_readFalse)
+			await category.set_permissions(inter.author, overwrite=overwrites_readFalse)
 			return await inter.reply("Private Chat Deleted.", ephemeral=True)
 		elif category is None:
 			return await inter.reply("You don't have a Private Chat", ephemeral=True)
