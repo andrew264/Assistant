fef7b8cad5ed5dc978942a8f14896b8f5ad6d57b
andrew264 <andrewsanthosh264@gmail.com>
2021-09-28 00:59:54 +0530

    whois:? in slash commands

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -1,7 +1,8 @@
-﻿import discord
-import discord.ext.commands as commands
+﻿import discord.ext.commands as commands
+from discord.utils import get
 from discord import Member, Embed
 from discord import Spotify, Game, CustomActivity, Streaming, Activity
+import dislash
 from dislash.application_commands import slash_client
 from dislash.interactions.application_command import Option, OptionType
 from dislash.interactions.app_command_interaction import SlashInteraction
@@ -12,26 +13,31 @@ class userinfo(commands.Cog):
 	def __init__(self,client):
 		self.client = client
 
-	@commands.command(aliases=['info', 'analyse', 'user', 'userinfo'])
-	@commands.guild_only()
-	@commands.bot_has_permissions(embed_links=True)
-	async def whois(self, ctx: commands.Context, *, user: Member = None):
+	@slash_client.slash_command(description="Luk wat he be doin ovar der",
+		options=[Option("user", "Mention someone", OptionType.USER)])
+	@dislash.guild_only()
+	@dislash.bot_has_permissions(embed_links=True)
+	async def whois(self, inter: SlashInteraction, user: Member = None):
 		if user is None:
-			user = ctx.author
+			user = inter.author
+
+		# Fetch activity. I really wish i don't have to do this.
+		userWithPresence: Member = get(self.client.get_all_members(), id=user.id)
+
 		date_format = "%a, %d %b %Y %I:%M %p"
 		embed = Embed(color=user.colour, description=user.mention)
 		embed.set_author(name=user, icon_url=user.display_avatar.url)
 		embed.set_thumbnail(url=user.display_avatar.url)
 		time_now = datetime.now(timezone.utc)
-		embed.add_field(name=f"Joined {ctx.guild.name} on", value=f"{user.joined_at.strftime(date_format)}\n**({(time_now - user.joined_at).days} days ago)**")
+		embed.add_field(name=f"Joined {inter.guild.name} on", value=f"{user.joined_at.strftime(date_format)}\n**({(time_now - user.joined_at).days} days ago)**")
 		embed.add_field(name="Account created on", value=f"{user.created_at.strftime(date_format)}\n**({(time_now - user.created_at).days} days ago)**")
 		if user.nick is not None:
 			embed.add_field(name="Nickname", value=user.nick)
 		# Clients
-		if user.raw_status != 'offline':
-			embed.add_field(name="Available Clients", value=userinfo.AvailableClients(user))
+		if userWithPresence.raw_status != 'offline':
+			embed.add_field(name="Available Clients", value=userinfo.AvailableClients(userWithPresence))
 		# Activity
-		for activity in user.activities:
+		for activity in userWithPresence.activities:
 			if isinstance(activity, Game):
 				embed.add_field(name="Playing", value=userinfo.ActivityVal(activity))
 			elif isinstance(activity, Streaming):
@@ -49,21 +55,23 @@ class userinfo(commands.Cog):
 			role_string = ' '.join([r.mention for r in user.roles][1:])
 			embed.add_field(name=f"Roles [{len(user.roles)-1}]", value=role_string, inline=False)
 		embed.set_footer(text=f'User ID: {user.id}')
-		try:
-			return await ctx.send(embed=embed)
-		except discord.Forbidden:
-			return await ctx.send("Missing Embed Permission.")
+		return await inter.send(embed=embed)
 
 	def ActivityVal(activity: Activity):
-		value: str = f"**{activity.name}** "
+		value: str = f"**{activity.name}**\n"
 		if activity.start is not None:
-			delta = (datetime.now(timezone.utc) - activity.start).seconds
-			if delta < 60:
-				value += f"({delta} s)"
-			elif 60 <= delta < 3600:
-				value += f"({delta//60} mins {delta%60} sec)"
-			elif delta >=3600:
-				value += f"({delta//3600} hrs {(delta//60)%60} mins)"
+			value += userinfo.timeDelta(activity.start)
+		return value
+
+	def timeDelta(timestamp: datetime):
+		sec = (datetime.now(timezone.utc) - timestamp).seconds
+		value:str = ""
+		if sec < 60:
+			value += f"({sec} s)"
+		elif 60 <= sec < 3600:
+			value += f"({sec//60} mins {sec%60} sec)"
+		elif sec >=3600:
+			value += f"({sec//3600} hrs {(sec//60)%60} mins)"
 		return value
 
 	def CustomActVal(activity: CustomActivity):
@@ -71,7 +79,8 @@ class userinfo(commands.Cog):
 		if activity.emoji is not None:
 			value += f"[{activity.emoji}]({activity.emoji.url}) "
 		if activity.name is not None:
-			value += activity.name
+			value += f"**{activity.name}**"
+		value += f"\n{userinfo.timeDelta(activity.created_at)}"
 		return value
 
 	def AvailableClients(user: Member):
