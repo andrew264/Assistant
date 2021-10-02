333f4baf9ccc8c32aed0ab832ad6a8597dcebd10
andrew264 <andrewsanthosh264@gmail.com>
2021-10-02 10:15:13 +0530

    update help

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,4 @@
-# discord stuff
+﻿# discord stuff
 from discord.ext import commands
 from discord import Embed, Colour
 from dislash.application_commands import slash_client
@@ -13,29 +13,33 @@ class HelpMe(commands.Cog):
 	def __init__(self,client):
 		self.client = client
 
-	@slash_client.slash_command(description="How may I help you")
+	@slash_client.slash_command(description="How may I help you ?")
 	async def help(self, inter):
 		row = ActionRow(
-            Button(style=ButtonStyle.blurple, label="User Info", custom_id="user"),
+            Button(style=ButtonStyle.blurple, label="General Commands", custom_id="general"),
             Button(style=ButtonStyle.blurple, label="Cleaning Commands", custom_id="clean"),
-            Button(style=ButtonStyle.blurple, label="Music Commands", custom_id="music")
+            Button(style=ButtonStyle.blurple, label="Music Commands", custom_id="music"),
+            Button(style=ButtonStyle.blurple, label="Fun Commands", custom_id="fun")
         )
-		msg = await inter.reply('Slash Commands are not listed here.', components=[row])
+		msg = await inter.reply('How may I help you ?', components=[row])
 		on_click = msg.create_click_listener(timeout=120)
 
-		### User Embed 
-		userembed = Embed(color = Colour.blurple())
-		userembed.set_author(name='User Info',icon_url=self.client.user.avatar.url)
-		userembed.add_field(name='`+whois`\t<@User>\t\t\tUser\'s Info', value='\u200b', inline=False)
-		userembed.add_field(name='`+aboutme`\t\t\t\t\t\t\t  Bot\'s Info', value='\u200b', inline=False)
+		### general Embed 
+		generalembed = Embed(color = Colour.blurple())
+		generalembed.set_author(name='General Commands',icon_url=self.client.user.avatar.url)
+		generalembed.add_field(name='`/whois`', value='User\'s Info', inline=True)
+		generalembed.add_field(name='`/botinfo`', value='Bot\'s Info', inline=True)
+		generalembed.add_field(name='`/chat create`', value='Create a new Private Chat', inline=False)
+		generalembed.add_field(name='`/help`', value='Get this help message', inline=False)
+		generalembed.add_field(name='`/tts`', value='Generate a TTS message', inline=False)
 		###
 
 		### Chat Cleaning
 		cleanembed = Embed(color = Colour.blurple())
 		cleanembed.set_author(name='Cleanse The Chat',icon_url=self.client.user.avatar.url)
-		cleanembed.add_field(name='`+clear`\t<@User>\t<No_of_Msgs>\t\t  Delete No. of Msgs', value='\u200b', inline=False)
-		cleanembed.add_field(name='`+yeetill`\t<MessageID>\t\t\t\t\t\t\t   Delete till that Msg', value='\u200b', inline=False)
-		cleanembed.add_field(name='`+yeetmsg`\t<string>\t<No_of_Msgs>\t\tDelete No. of Msgs Containing that String', value='\u200b', inline=False)
+		cleanembed.add_field(name='`.clear`\t<@User>\t<No_of_Msgs>\t\t  Delete No. of Msgs', value='\u200b', inline=False)
+		cleanembed.add_field(name='`.yeetill`\t<MessageID>\t\t\t\t\t\t\t   Delete till that Msg', value='\u200b', inline=False)
+		cleanembed.add_field(name='`.yeetmsg`\t<string>\t<No_of_Msgs>\t\tDelete No. of Msgs Containing that String', value='\u200b', inline=False)
 		###
 
 		### Music Commands
@@ -46,20 +50,32 @@ class HelpMe(commands.Cog):
 		musicembed.add_field(name='`.stop`', value='Disconnect Bot from VC')
 		musicembed.add_field(name='`.np`', value='Display Now Playing')
 		musicembed.add_field(name='`.queue`', value='Songs in Queue')
-		musicembed.add_field(name='`+skip`\t<song_index>', value='Skip Songs')
-		musicembed.add_field(name='`+loop`', value='Toggle Loop')
-		musicembed.add_field(name='`+jump`\t<song_index>', value='Skip to a Song')
+		musicembed.add_field(name='`.skip`\t<song_index>', value='Skip Songs')
+		musicembed.add_field(name='`.loop`', value='Toggle Loop')
+		musicembed.add_field(name='`.jump`\t<song_index>', value='Skip to a Song')
 		###
 
-		@on_click.matching_id("user")
+		### Fun Commands
+		funembed = Embed(color = Colour.dark_orange())
+		funembed.set_author(name='Fun Stuff',icon_url=self.client.user.avatar.url)
+		funembed.add_field(name='`/kill`', value='Delete someone\'s existance', inline=False)
+		funembed.add_field(name='`/pp`', value='Measure someone in Inches 🤏', inline=False)
+		funembed.add_field(name='`/lyrics`', value='Get lyrics from Spotify Activity', inline=False)
+		funembed.add_field(name='`/ping`', value='Get Bot\'s Latency', inline=False)
+		###
+
+		@on_click.matching_id("general")
 		async def on_user(inter):
-			await inter.reply(embed = userembed, ephemeral=True)
+			await inter.reply(embed = generalembed, ephemeral=True)
 		@on_click.matching_id("clean")
 		async def on_clean(inter):
 			await inter.reply(embed = cleanembed, ephemeral=True)
 		@on_click.matching_id("music")
 		async def on_music(inter):
 			await inter.reply(embed = musicembed, ephemeral=True)
+		@on_click.matching_id("fun")
+		async def on_music(inter):
+			await inter.reply(embed = funembed, ephemeral=True)
 		@on_click.timeout
 		async def on_timeout():
 			await msg.edit(components=[])
