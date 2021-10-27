4647c4bc6da06cc873cf0ee7c5bcdc383c593139
andrew264 <andrewsanthosh264@gmail.com>
2021-10-27 13:29:57 +0530

    reuse existing web hooks

--- cogs/BotReply.py
+++ cogs/BotReply.py
@@ -28,10 +28,18 @@ class BotReply(commands.Cog):
 			return await BotReply.ReplyWebhook(self, message.channel, response)
 
 	async def ReplyWebhook(self, channel: TextChannel, response: str):
-		webhook: Webhook = await channel.create_webhook(name="Reply Hook")
+		webhook: Webhook = await BotReply.FetchHook(self, channel)
 		avatarURL = self.client.get_user(493025015445454868).display_avatar.url
 		await webhook.send(content=response, username='Andrew', avatar_url = avatarURL)
-		await webhook.delete()
+
+	async def FetchHook(self, channel: TextChannel):
+		channel_hooks = await channel.webhooks()
+		for webhook in channel_hooks:
+			if webhook.name == "Reply Hook":
+				return webhook
+
+		webhook: Webhook = await channel.create_webhook(name="Reply Hook")
+		return webhook
 
 def setup(client):
 	client.add_cog(BotReply(client))
