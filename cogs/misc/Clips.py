01d22953b687c49a57c8ad446d87d2a9dd7eb398
andrew264 <andrewsanthosh264@gmail.com>
2022-03-10 17:01:42 +0530

    clips: just works some how

--- cogs/misc/Clips.py
+++ cogs/misc/Clips.py
@@ -11,53 +11,60 @@ class Clips(commands.Cog):
     def __init__(self, client: Client):
         self.client = client
 
-    class SelectClip(disnake.ui.Select):
-        def __init__(self):
-            options = [disnake.SelectOption(label=clip) for clip in os.listdir("clips/") if clip.endswith(".mp3")]
-            super().__init__(
-                placeholder="Select a clip",
-                custom_id="clips",
-                min_values=1,
-                max_values=1,
-                options=options, )
-
-        async def callback(self, interaction: disnake.MessageInteraction):
-            await interaction.response.edit_message(content=f"Selected: {self.values[0]}")
-
-    class Buttons(disnake.ui.View):
-        def __init__(self, voice):
-            super().__init__(timeout=60)
-            self.select = Clips.SelectClip()
-            self.add_item(self.select)
-            self.voice: disnake.VoiceClient = voice
-
-        @disnake.ui.button(emoji="▶️", style=disnake.ButtonStyle.primary)
-        async def play(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-            if self.select.values:
-                clip = f"clips/{self.select.values[0]}"
-            else:
-                await interaction.response.edit_message(content="No clip selected", view=self)
-                return
-            if self.voice.is_playing:
-                self.voice.stop()
-            self.voice.play(disnake.FFmpegPCMAudio(clip))
-            await interaction.response.edit_message(content=f"Playing {self.select.values[0]}...")
-
-        @disnake.ui.button(emoji="⏹️", style=disnake.ButtonStyle.danger)
-        async def stop(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
-            if self.voice:
-                await self.voice.disconnect(force=True)
-            await interaction.response.edit_message(content="Bye have a great time", view=None)
-
     @commands.slash_command(name="clips")
     @commands.guild_only()
     async def clips(self, inter: disnake.ApplicationCommandInteraction) -> None:
         """Play mp3 in Voice Channel """
         voice: Optional[disnake.VoiceClient] = disnake.utils.get(self.client.voice_clients, guild=inter.guild)
+
+        class ClipView(disnake.ui.View):
+            def __init__(self):
+                super().__init__(timeout=60)
+
+                class ClipDropdown(disnake.ui.Select):
+                    def __init__(self):
+                        super().__init__(placeholder="Select a clip", custom_id="clips", min_values=1, max_values=1,
+                                         options=[disnake.SelectOption(label=clip) for clip in os.listdir('clips/') if
+                                                  clip.endswith('.mp3')], )
+
+                    async def callback(self, interaction: disnake.MessageInteraction):
+                        await interaction.response.edit_message(content=f"Selected: {self.values[0]}")
+
+                self.add_item(ClipDropdown())
+
+            async def on_timeout(self) -> None:
+                await voice.disconnect(force=True)
+                try:
+                    await inter.edit_original_message(content="Bye have a great time", view=None)
+                except disnake.HTTPException:
+                    pass
+
+            @disnake.ui.button(emoji="▶️", style=disnake.ButtonStyle.primary)
+            async def play(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
+                for child in self.children:
+                    if isinstance(child, disnake.ui.Select):
+                        if child.values:
+                            clip = child.values[0]
+                            break
+                else:
+                    await interaction.response.edit_message(content="No clip selected", view=self)
+                    return
+                if voice.is_playing:
+                    voice.stop()
+                voice.play(disnake.FFmpegPCMAudio(f"clips/{clip}"))
+                await interaction.response.edit_message(content=f"Playing {clip}...")
+
+            @disnake.ui.button(emoji="⏹️", style=disnake.ButtonStyle.danger)
+            async def stop(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
+                if voice:
+                    await voice.disconnect(force=True)
+                await interaction.response.edit_message(content="Bye have a great time", view=None)
+
+        await inter.response.send_message("Select a clip", view=ClipView())
+
         if voice is None:
             voice_channel = inter.author.voice.channel
             voice: disnake.VoiceClient = await voice_channel.connect()
-        await inter.response.send_message("Select a clip", view=Clips.Buttons(voice))
 
     # Checks
     @clips.before_invoke
