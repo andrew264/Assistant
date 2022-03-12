255f0b18fb57ae0e29fbe5fdb94b2e0ccdf1a7eb
andrew264 <andrewsanthosh264@gmail.com>
2022-03-12 16:47:44 +0530

    dm: small improvements

---
+++ cogs/admin/dm.py
@@ -0,0 +1,51 @@
+﻿# Imports
+from typing import List
+
+import disnake
+from disnake.ext import commands
+
+from EnvVariables import DM_Channel
+from assistant import Client, getch_hook
+
+
+class OnDM(commands.Cog):
+    def __init__(self, client: Client):
+        self.client = client
+
+    # Replies
+    @commands.Cog.listener()
+    async def on_message(self, message: disnake.Message) -> None:
+        if message.guild or message.author.bot:
+            return
+        msg_content = f"UserID: `{message.author.id}`\n"
+        if message.content:
+            msg_content += f"Content: ```{message.content}```\n"
+        files: List[disnake.File] = [await attachment.to_file() for attachment in message.attachments]
+        webhook = await getch_hook(self.client.get_channel(DM_Channel))
+        await webhook.send(content=msg_content, files=files,
+                           username=str(message.author), avatar_url=message.author.display_avatar.url)
+
+    # slide to dms
+    @commands.command()
+    @commands.is_owner()
+    async def dm(self, ctx: commands.Context, user: disnake.User, *, msg: str = "") -> None:
+        files: List[disnake.File] = [await attachment.to_file() for attachment in ctx.message.attachments]
+        if not msg and not files:
+            await ctx.send("Please provide a message or an attachment")
+            return
+        try:
+            channel = await user.create_dm()
+            await channel.send(content=msg if msg else None, files=files)
+        except (disnake.Forbidden | disnake.HTTPException):
+            await ctx.send(f"Failed to DM {user}.")
+        else:
+            await ctx.send(f"DM'd {user}.")
+
+        # log Sent Direct Messages
+        dm = self.client.get_channel(DM_Channel)
+        await dm.send(content=f"From: **{ctx.author}**\nTo: **{user}**.\nContent: *{msg}*", files=files, )
+        await ctx.message.delete()
+
+
+def setup(client):
+    client.add_cog(OnDM(client))
