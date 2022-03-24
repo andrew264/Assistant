7071cf7cb8b365d0027d053b8937d694e38dec64
andrew264 <andrewsanthosh264@gmail.com>
2022-03-24 14:36:12 +0530

    2/? add lastseen

--- cogs/tasks/last_seen.py
+++ cogs/tasks/last_seen.py
@@ -1,3 +1,8 @@
+import datetime
+from typing import Optional
+
+import aiosqlite
+import disnake
 from disnake.ext import commands
 
 from assistant import Client
@@ -6,6 +11,25 @@ from assistant import Client
 class LastSeen(commands.Cog):
     def __init__(self, client: Client):
         self.client = client
+        self.db: Optional[aiosqlite.Connection] = None
+
+    @commands.Cog.listener()
+    async def on_presence_update(self, before: disnake.Member, after: disnake.Member) -> None:
+        if before.bot:
+            return
+        if before.raw_status != 'offline' or after.raw_status != 'offline':
+            return
+        self.db = await self.client.db_connect()
+        user_id = before.id
+        timestamp = datetime.datetime.now().timestamp()
+        async with self.db.execute(f"SELECT * FROM Members WHERE USERID = {user_id}") as cursor:
+            value = await cursor.fetchone()
+            if value:
+                await self.db.execute(f"UPDATE Members SET last_seen = '{timestamp}' WHERE USERID = {user_id}")
+            else:
+                await self.db.execute("INSERT INTO Members (USERID, last_seen) VALUES (?,?)", (user_id, timestamp))
+            if self.db.total_changes:
+                await self.db.commit()
 
 
 def setup(client: Client):
