820840763c78bb8c6fc67f87d1a207d9ab2b835e
andrew264 <andrewsanthosh264@gmail.com>
2022-03-25 17:01:20 +0530

    last_seen: catch yall invisible people when possible

--- cogs/tasks/last_seen.py
+++ cogs/tasks/last_seen.py
@@ -13,16 +13,9 @@ class LastSeen(commands.Cog):
         self.client = client
         self.db: Optional[aiosqlite.Connection] = None
 
-    @commands.Cog.listener()
-    async def on_presence_update(self, before: disnake.Member, after: disnake.Member) -> None:
-        if before.bot:
-            return
-        if (before.raw_status == after.raw_status) or \
-                before.raw_status != 'offline' and after.raw_status != 'offline':
-            return
+    async def update_time(self, user_id: int) -> None:
         if self.db is None:
             self.db = await self.client.db_connect()
-        user_id = before.id
         timestamp = int(datetime.datetime.now().timestamp())
         async with self.db.execute(f"SELECT * FROM Members WHERE USERID = {user_id}") as cursor:
             value = await cursor.fetchone()
@@ -32,6 +25,49 @@ class LastSeen(commands.Cog):
                 await self.db.execute("INSERT INTO Members (USERID, last_seen) VALUES (?,?)", (user_id, timestamp))
             if self.db.total_changes:
                 await self.db.commit()
+
+    @commands.Cog.listener()
+    async def on_presence_update(self, before: disnake.Member, after: disnake.Member) -> None:
+        if before.bot:
+            return
+        if (before.raw_status == after.raw_status) or \
+                before.raw_status != 'offline' and after.raw_status != 'offline':
+            return
+        await self.update_time(after.id)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: disnake.Message) -> None:
+        author = message.author
+        if author.bot:
+            return
+        if author.raw_status != 'offline':
+            return
+        await self.update_time(author.id)
+
+    @commands.Cog.listener()
+    async def on_typing(self, channel: disnake.TextChannel, user: disnake.Member, when: datetime) -> None:
+        if isinstance(channel, disnake.DMChannel):
+            return
+        if user.bot:
+            return
+        if user.raw_status != 'offline':
+            return
+        await self.update_time(user.id)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: disnake.Member,
+                                    before: disnake.VoiceState, after: disnake.VoiceState) -> None:
+        if member.bot:
+            return
+        if member.raw_status != 'offline':
+            return
+        await self.update_time(member.id)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: disnake.Member) -> None:
+        if member.bot:
+            return
+        await self.update_time(member.id)
 
 
 def setup(client: Client):
