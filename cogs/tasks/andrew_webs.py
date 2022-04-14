1ee5728840362d272eecbcc4da3fc91d42c1c46f
andrew264 <andrewsanthosh264@gmail.com>
2022-04-14 21:28:00 +0530

    andrewWebs: trying out new stuff

--- cogs/tasks/andrew_webs.py
+++ cogs/tasks/andrew_webs.py
@@ -5,22 +5,25 @@ from random import choice, randint
 import disnake
 import emoji
 from disnake.ext import commands
+from thefuzz import process, fuzz
 
 from EnvVariables import Owner_ID
 from assistant import Client, getch_hook
 
-references = ["andrew",
+references = ("andrew",
               "santhosh",
               "@andrew",
               "@andrew!#1901",
               "<@!493025015445454868>",
               493025015445454868,
               "<:datsshaawt:804242984383545344>",
-              "<:andrew_damnboii:794247753445802004>", ]
+              "<:andrew_damnboii:794247753445802004>",)
 
 with open("data/AndrewReplies.json", "r") as replyJSON:
     replies = json.load(replyJSON)
-    keys = list(replies.keys())
+    ALL_QN = []
+    for _key in replies:
+        ALL_QN.extend(replies[_key]["QN"])
     replyJSON.close()
 
 
@@ -42,6 +45,12 @@ def checks(message: disnake.Message) -> bool:
     return False
 
 
+def fetch_reply(qn: str) -> str:
+    for key in replies:
+        if qn in replies[key]["QN"]:
+            return choice(replies[key]["ANS"])
+
+
 class AndrewWebs(commands.Cog):
     def __init__(self, client: Client):
         self.client = client
@@ -50,19 +59,25 @@ class AndrewWebs(commands.Cog):
     async def on_message(self, message: disnake.Message) -> None:
         if checks(message) is False:
             return
-        member: disnake.Member = message.guild.get_member(Owner_ID)
 
-        # Replies
-        for word in message.clean_content.lower().split():
-            if word in keys:
-                response = replies[word]
-                await self.reply_hook(message.channel, member, response)
-                return
+        # Clean up references
+        msg = ""
+        for word in message.content.lower().split():
+            if word not in references:
+                msg += word + " "
+
+        # Fuzzy search for reply
+        match, score = process.extractOne(query=msg, choices=ALL_QN,
+                                          scorer=fuzz.token_sort_ratio, score_cutoff=60)
+        self.client.logger.debug(f"{msg} | {match} | {score}")
+        if match is not None:
+            await self.reply_hook(message.channel, fetch_reply(match))
+            return
 
         # Emoji tings
         if any(word in message.content.lower().split() for word in emoji.UNICODE_EMOJI_ENGLISH):
             response = choice(["👍", "😂", "🥲", "🤨", "🙄", "😏", "👽", "💩", "🤌", "🤝"])
-            await self.reply_hook(message.channel, member, response=f"{response * randint(1, 7)}")
+            await self.reply_hook(message.channel, response=f"{response * randint(1, 7)}")
             return
 
         # just mentions
@@ -71,16 +86,14 @@ class AndrewWebs(commands.Cog):
                 if message.content.lower() == i:
                     response = choice(["No Not ME", "Yes Tell Me",
                                        "What ?", "Any Problem ?",
-                                       "Why me ?", "yup yup", ])
-                    await self.reply_hook(message.channel, member, response)
+                                       "Why me ?", ])
+                    await self.reply_hook(message.channel, response)
                     return
 
-        # await message.add_reaction("🤔")
-        await self.reply_hook(message.channel, member, choice(["😏", "🙄", "mmm", "huh"]))
-
     @staticmethod
-    async def reply_hook(channel: disnake.TextChannel, member: disnake.Member, response: str):
+    async def reply_hook(channel: disnake.TextChannel, response: str):
         webhook: disnake.Webhook = await getch_hook(channel)
+        member: disnake.Member = channel.guild.get_member(Owner_ID)
         await webhook.send(content=response, username=member.display_name, avatar_url=member.display_avatar.url)
 
 
