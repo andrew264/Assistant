f9a4a3313fb3a6bd4284cdc44a4355da64916cd8
andrew264 <andrewsanthosh264@gmail.com>
2022-12-31 16:18:25 +0530

    admin: extensions: manage cogs with slash commands

--- cogs/admin/extensions.py
+++ cogs/admin/extensions.py
@@ -1,74 +1,76 @@
+import itertools
+
+import disnake
 from disnake.ext import commands
 
-import assistant
+from config import home_guild
+from assistant import Client
 
 
 class Extensions(commands.Cog):
-    def __init__(self, client: assistant.Client):
-        self.client = client
-        self.logger = client.logger
+    def __init__(self, client: Client):
+        self.bot = client
+
+    @commands.slash_command(name="extensions", description="Manage extensions.", guild_ids=[home_guild])
+    async def extensions(self, inter: disnake.ApplicationCommandInteraction) -> None:
+        if inter.user.id != self.bot.owner_id:
+            return await inter.response.send_message("You are not allowed to use this command.", ephemeral=True)
 
     # Load Extension
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def load(self, ctx: commands.Context, extension) -> None:
+    @extensions.sub_command(name="load", description="Load an extension.")
+    async def load(self, inter: disnake.ApplicationCommandInteraction,
+                   extension: str = commands.Param(description="Extension to Load.")) -> None:
         try:
-            self.client.load_extension(f"cogs.{extension}")
+            self.bot.load_extension(f"cogs.{extension}")
         except Exception as e:
-            await ctx.message.add_reaction("☠️")
-            await ctx.send(f"{type(e).__name__}: {e}")
-            self.logger.warning(f"Failed to load extension: cogs.{extension}")
+            await inter.response.send_message(f"{type(e).__name__}: {e}")
+            print(f"Failed to load extension: cogs.{extension}")
         else:
-            await ctx.message.add_reaction("✅")
-            self.logger.info(f"Loaded extension: cogs.{extension}")
+            await inter.response.send_message(f"Loaded extension: `cogs.{extension}`")
+            print(f"Loaded extension: cogs.{extension}")
 
     # Unload Extension
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def unload(self, ctx: commands.Context, extension) -> None:
+    @extensions.sub_command(name="unload", description="Unload an extension.")
+    async def unload(self, inter: disnake.ApplicationCommandInteraction,
+                     extension: str = commands.Param(description="Select an Extension to Unload.")) -> None:
         try:
-            self.client.unload_extension(f"cogs.{extension}")
+            self.bot.unload_extension(extension)
         except Exception as e:
-            await ctx.message.add_reaction("☠️")
-            await ctx.send(f"{type(e).__name__}: {e}")
-            self.logger.warning(f"Failed to unload extension: cogs.{extension}")
+            await inter.response.send_message(f"{type(e).__name__}: {e}")
+            print(f"Failed to unload extension: {extension}")
         else:
-            await ctx.message.add_reaction("✅")
-            self.logger.info(f"Unloaded extension: cogs.{extension}")
+            await inter.response.send_message(f"Unloaded extension: `{extension}`")
+            print(f"Unloaded extension: {extension}")
 
     # Reload Extension
-    @commands.command(hidden=True)
-    @commands.is_owner()
-    async def reload(self, ctx: commands.Context, extension) -> None:
+    @extensions.sub_command(name="reload", description="Reload an extension.")
+    async def reload(self, inter: disnake.ApplicationCommandInteraction,
+                     extension: str = commands.Param(description="Select a Extension to Reload.")) -> None:
         try:
-            self.client.reload_extension(f"cogs.{extension}")
+            if extension == "all":
+                for extension in self.bot.extensions:
+                    self.bot.reload_extension(extension)
+            else:
+                self.bot.reload_extension(extension)
         except Exception as e:
-            await ctx.message.add_reaction("☠️")
-            await ctx.send(f"{type(e).__name__}: {e}")
-            self.logger.warning(f"Failed to reload extension: cogs.{extension}")
+            await inter.response.send_message(f"{type(e).__name__}: {e}")
+            print(f"Failed to reload extension: {extension}")
         else:
-            await ctx.message.add_reaction("✅")
-            self.logger.info(f"Reloaded extension: cogs.{extension}")
+            await inter.response.send_message(f"Reloaded extension: `{extension}`")
+            print(f"Reloaded extension: {extension}")
 
-    @commands.command(name="reloadall", hidden=True)
-    @commands.is_owner()
-    async def reload_all(self, ctx: commands.Context) -> None:
-        try:
-            for extension in self.client.extensions:
-                self.client.reload_extension(extension)
-        except Exception as e:
-            await ctx.message.add_reaction("☠️")
-            await ctx.send(f"{type(e).__name__}: {e}")
-            self.logger.warning(f"Failed to reload all extensions")
-        else:
-            await ctx.message.add_reaction("✅")
-            self.logger.info(f"Reloaded all extensions")
+    @extensions.sub_command(name="list", description="List all extensions.")
+    async def list_extensions(self, inter: disnake.ApplicationCommandInteraction) -> None:
+        await inter.response.send_message(f"```{', '.join(self.bot.extensions.keys())}```")
 
-    @commands.command(name="listext", hidden=True)
-    @commands.is_owner()
-    async def list_extensions(self, ctx: commands.Context) -> None:
-        await ctx.send(f"```{', '.join(self.client.extensions.keys())}```")
+    @unload.autocomplete('extension')
+    @reload.autocomplete('extension')
+    def extension_auto_complete(self, inter: disnake.ApplicationCommandInteraction,
+                                extension: str) -> dict[str, str]:
+
+        extensions = {ext[5:]: ext for ext in self.bot.extensions.keys() if extension in ext}
+        return dict(itertools.islice(extensions.items(), 25))
 
 
-def setup(client):
+def setup(client: Client) -> None:
     client.add_cog(Extensions(client))
