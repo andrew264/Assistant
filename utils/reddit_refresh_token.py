2e6cb09e27f16c9176a0cfe8327d1c75e5a63c83
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-05-18 18:53:20 +0530

    reddit: get refresh token

---
+++ utils/reddit_refresh_token.py
@@ -0,0 +1,64 @@
+import random
+import socket
+import sys
+
+import praw
+
+from config import RedditConfig
+
+cfg = RedditConfig()
+CLIENT_ID = cfg.CLIENT_ID
+CLIENT_SECRET = cfg.CLIENT_SECRET
+USERNAME = cfg.USERNAME
+REDIRECT_URI = 'http://localhost:8080/'
+DURATION = 'permanent'
+SCOPES = ["read"]
+STATE = str(random.randint(0, 65000))
+
+
+def main():
+    """Provide the program's entry point when directly executed."""
+
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        user_agent=f"get refresh token/v0 by u/{USERNAME}",
+    )
+    url = reddit.auth.url(duration=DURATION, scopes=SCOPES, state=STATE)
+    print(f"Now open this url in your browser: {url}")
+
+    client = receive_connection()
+    data = client.recv(1024).decode("utf-8")
+    print(data)
+    print("_" * 80)
+    param_tokens = data.split(" ", 2)[1].split("?", 1)[1].split("&")
+    params = {
+        key: value for (key, value) in [token.split("=") for token in param_tokens]
+    }
+
+    refresh_token = reddit.auth.authorize(params["code"])
+    send_message(client, f"Refresh token: {refresh_token}")
+    return 0
+
+
+def receive_connection():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("localhost", 8080))
+    server.listen(1)
+    client = server.accept()[0]
+    server.close()
+    return client
+
+
+def send_message(client, message):
+    """Send message to client and close the connection."""
+    print(message)
+    print("_" * 80)
+    client.send(f"HTTP/1.1 200 OK\r\n\r\n{message}".encode("utf-8"))
+    client.close()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
